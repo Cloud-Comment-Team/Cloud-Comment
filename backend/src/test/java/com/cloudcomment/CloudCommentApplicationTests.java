b71987273050652759ec2511c30f99c2cc247a3a
IWKMS99 <alievsaid0909@yandex.ru>
2026-07-13 02:58:41 +0300

    fix: disable unused default security user (#157)

--- backend/src/test/java/com/cloudcomment/CloudCommentApplicationTests.java
+++ backend/src/test/java/com/cloudcomment/CloudCommentApplicationTests.java
@@ -1,12 +1,21 @@
 package com.cloudcomment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(properties = "spring.flyway.enabled=false")
 class CloudCommentApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext.getBeansOfType(InMemoryUserDetailsManager.class)).isEmpty();
     }
 }
